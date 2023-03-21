9fe5f3dfd97331e586a2c696a6c9393f624bb614
Jonald Damaso <damasojonald@gmail.com>
2023-03-21 15:37:21 +0000

    build: installed react-router-dome

---
+++ client/src/components/header.tsx
@@ -0,0 +1,16 @@
+function Header(): JSX.Element {
+    return (
+        <>
+            <header className='header__container'>
+                <h1 className='header__title header__welcome'>Welcome to the Home of the Justice Department of Fakelandia</h1>
+                <h2 className='header__title'>
+                    Here you can browse a list of recent misdemeanours committed by our citizens, or you can confess to your own 
+                    misdemeanours.
+                </h2>
+            </header>
+        </>
+    )
+	
+}
+
+export default Header;
